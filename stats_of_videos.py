51cbb3c04351affbca4606fb6ec5402d09dd1cdb
eliel <elielak@campus.technion.ac.il>
2022-12-03 11:08:25 +0200

    getting stats of the video using only the GT/predicted csv of the video output.

---
+++ stats_of_videos.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import argparse
+from video import get_video_stats
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--video_csv_path', type=str, help='model path')
+    parser.add_argument('--output_folder', type=str, help='model path')
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.video_csv_path, index_col='frame_number')
+
+    get_video_stats(df, args.output_folder)
+
+
+if __name__ == '__main__':
+    main()
